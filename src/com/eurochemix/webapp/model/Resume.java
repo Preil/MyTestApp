5648e02b3a06c1a194a7ec792de92ed400818902
Preil <preil@yandex.ru>
2016-02-09 09:08:48 +0200

    End of lesson 01

---
+++ src/com/eurochemix/webapp/model/Resume.java
@@ -0,0 +1,15 @@
+package com.eurochemix.webapp.model;
+
+import java.util.Collection;
+
+/**
+ * Created by Ilya on 08.02.2016.
+ */
+public class Resume {
+
+    String fullName;
+    String[] something;
+    Collection<String> collection;
+
+
+}
