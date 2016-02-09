d04e9b739ac747b300c53c040fc87d35ce45d7fa
Preil <preil@yandex.ru>
2016-02-09 22:27:07 +0200

    Resume continue. Lesson02

---
+++ src/com/eurochemix/webapp/model/Section.java
@@ -0,0 +1,12 @@
+package com.eurochemix.webapp.model;
+
+/**
+ * Created by Ilya on 09.02.2016.
+ */
+public class Section {
+    private final String name;
+
+    public Section(String name) {
+        this.name = name;
+    }
+}
