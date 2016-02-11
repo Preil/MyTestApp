20b0aa11e1b14fe1381034ac4854dd1a65ec24e1
Preil <preil@yandex.ru>
2016-02-11 20:56:47 +0200

    Lesson3. adding classes to resume model. Using enums. Added ContactType and SectionType via enum variables type.

--- src/com/eurochemix/webapp/model/Section.java
+++ src/com/eurochemix/webapp/model/Section.java
@@ -4,9 +4,9 @@ package com.eurochemix.webapp.model;
  * Created by Ilya on 09.02.2016.
  */
 public class Section {
-    private final String name;
+    private final SectionType type;
 
     public Section(String name) {
-        this.name = name;
+        this.type = name;
     }
 }
