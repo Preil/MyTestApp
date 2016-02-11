8a7fd145f8c8c496c8c870c1b4b663db2b69dad6
Preil <preil@yandex.ru>
2016-02-11 09:08:23 +0200

    Resume continue. Lesson02

--- src/com/eurochemix/webapp/model/Main.java
+++ src/com/eurochemix/webapp/model/Main.java
@@ -8,12 +8,30 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!!");
-        Link l1 = new Link("Eurochemix","eurochemix.com");
+        Link l1 = new Link("Eurochemix", "eurochemix.com");
         Link l2 = l1;
         Link l3 = new Link(l1);
 
         System.out.println(l1.equals(l2));
         System.out.println(l1.equals(l3));
+        System.out.println(l1);
+
+        System.out.println(Link.EMPTY);
+        System.out.println(l1.empty());
+
+        Integer obj = new Integer(5);
+        printInt(5);
+
+
+    }
+
+    private static void printInt(Integer obj) {
+        System.out.println("Object: " + obj);
+
+    }
+
+    private static void printInt(int i) {
+        System.out.println("Integer: " + i);
 
     }
 }
