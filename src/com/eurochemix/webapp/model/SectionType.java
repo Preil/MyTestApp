4bf6d495ffa498131c4670ca7b9673075cc8567b
Preil <preil@yandex.ru>
2016-02-12 09:13:23 +0200

    SectionType title added.

--- src/com/eurochemix/webapp/model/SectionType.java
+++ src/com/eurochemix/webapp/model/SectionType.java
@@ -4,10 +4,19 @@ package com.eurochemix.webapp.model;
  * Created by Ilya on 11.02.2016.
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIVEMENT,
-    QUALIFICATIONS,
-    EXPIRIENCE,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ACHIVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPIRIENCE("Опыт работы"),
+    EDUCATION("Образование");
 
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
