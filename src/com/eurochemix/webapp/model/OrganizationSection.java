f7db26bfe98bfc3f0bf52decfbb1a4ea9ab8d147
Preil <preil@yandex.ru>
2016-02-12 08:56:49 +0200

    Implemented  model and diagram.

--- src/com/eurochemix/webapp/model/OrganizationSection.java
+++ src/com/eurochemix/webapp/model/OrganizationSection.java
@@ -1,9 +1,11 @@
 package com.eurochemix.webapp.model;
 
+import java.util.List;
+
 /**
  * Created by Ilya on 11.02.2016.
  */
-public class OrganizationSection {
-
+public class OrganizationSection extends Section {
+    private List<Organization> values;
 
 }
--- src/com/eurochemix/webapp/model/TextSection.java
+++ src/com/eurochemix/webapp/model/TextSection.java
@@ -3,7 +3,7 @@ package com.eurochemix.webapp.model;
 /**
  * Created by Ilya on 11.02.2016.
  */
-public class TextSection {
+public class TextSection extends Section {
     private String title;
     private String comment;
 }
