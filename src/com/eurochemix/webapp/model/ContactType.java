655d3a22422662cf58063376ab0ab80b86727b02
Preil <preil@yandex.ru>
2016-02-12 09:30:03 +0200

    ContactType title added.

--- src/com/eurochemix/webapp/model/ContactType.java
+++ src/com/eurochemix/webapp/model/ContactType.java
@@ -4,12 +4,21 @@ package com.eurochemix.webapp.model;
  * Created by Ilya on 11.02.2016.
  */
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PHONE,
-    SKYPE,
-    MAIL,
-    ICQ,
-    WEB_PAGE
+    PHONE("Контактный телефон"),
+    MOBILE("Мобильный телефон"),
+    HOME_PHONE("Домашний телефон"),
+    SKYPE("Skype"),
+    MAIL("Почта"),
+    ICQ("ICQ"),
+    WEB_PAGE("Web page");
 
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
