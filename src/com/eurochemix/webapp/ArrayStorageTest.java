c40ff5f48583eb74eddef92b31edf1d004c4a8dd
Preil <preil@yandex.ru>
2016-02-15 16:42:09 +0200

    Test Junit4 ArrayStorage 34:35

---
+++ src/com/eurochemix/webapp/ArrayStorageTest.java
@@ -0,0 +1,44 @@
+package com.eurochemix.webapp;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ilya on 15.02.2016.
+ */
+public class ArrayStorageTest {
+
+    @org.junit.Test
+    public void testClear() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSave() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLoad() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetAllSorted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSize() throws Exception {
+
+    }
+}
