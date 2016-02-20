be70b43792bd10dd28b3d8e3caa438726c9f3edb
Preil <preil@yandex.ru>
2016-02-20 08:52:00 +0200

    Uddate&delete metods updated in ArrayStorage.

--- src/com/eurochemix/webapp/ArrayStorage.java
+++ src/com/eurochemix/webapp/ArrayStorage.java
@@ -2,42 +2,51 @@ package com.eurochemix.webapp;
 
 import com.eurochemix.webapp.model.Resume;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 /**
  * Created by Ilya on 14.02.2016.
  */
 public class ArrayStorage implements IStorage {
     public static final int LIMIT = 100;
+    //    protected Logger LOGGER = Logger.getLogger(ArrayStorage.class.getName());
+    private static Logger LOGGER = Logger.getLogger(ArrayStorage.class.getName());
+
     private Resume[] array = new Resume[LIMIT];
+    private int size = 0;
+
 
     @Override
     public void clear() {
-//        array[i]=null;
+        LOGGER.info("Delete all resumes.");
+        Arrays.fill(array, null);
+        size = 0;
     }
 
     @Override
     public void save(Resume r) {
-        for (int i = 0; i < LIMIT; i++) {
-            Resume resume = array[i];
-            if (array[i] != null) {
-                if (r.equals(resume)) {
-                    throw new IllegalStateException("Already present");
-                }
-            }
-        }
-        for (int i = 0; i < LIMIT; i++) {
-            if (array[i] == null) {
-                array[i] = r;
-            }
-        }
-
-
+        LOGGER.info("Save resume = " + r.getUuid());
+        int idx = getIndex(r.getUuid());
+        if (idx != -1) throw new WebAppException("Resume " + r.getUuid() + " already exist", r);
+        array[size++] = r;
+//
+//
+//          try {
+//                throw new WebAppException("Resume " + r.getUuid() + " already exist", r);
+//            } catch (WebAppException e) {
+//                    LOGGER.severe(e.getMessage());
+//            }
+//        }
     }
 
     @Override
     public void update(Resume r) {
-
+        LOGGER.info("Update resume with " + r.getUuid());
+        int idx = getIndex(r.getUuid());
+        if (idx == -1) throw new WebAppException("Resume " + r.getUuid() + " not exist", r);
+        array[idx] = r;
     }
 
     @Override
@@ -47,6 +56,12 @@ public class ArrayStorage implements IStorage {
 
     @Override
     public void delete(String uuid) {
+        LOGGER.info("Delete resume with " + uuid);
+        int idx = getIndex(uuid);
+        if (idx == -1) throw new WebAppException("Resume " + uuid + " not exist");
+        int numMove = size - idx -1;
+        if (numMove<0) System.arraycopy(array,idx+1,array,idx, numMove);
+        array[--size]=null;
 
     }
 
@@ -63,5 +78,15 @@ public class ArrayStorage implements IStorage {
 //        }
 
         return 0;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < LIMIT; i++) {
+            if (array[i] != null) {
+                if (array[i].getUuid().equals(uuid)) return i;
+
+            }
+        }
+        return -1;
     }
 }
