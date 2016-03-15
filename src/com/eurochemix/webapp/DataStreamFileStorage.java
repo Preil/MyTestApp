a77bdda2ad0e05726953b93c4411ff96da12ac23
Preil <preil@yandex.ru>
2016-03-15 10:06:44 +0200

    Add web.xml

--- src/com/eurochemix/webapp/DataStreamFileStorage.java
+++ src/com/eurochemix/webapp/DataStreamFileStorage.java
@@ -11,7 +11,6 @@ import java.util.*;
  * Created by Ilya on 28.02.2016.
  */
 public class DataStreamFileStorage extends FileStorage {
-    private static final String NULL = "null";
 
     public DataStreamFileStorage(String path) {
         super(path);
@@ -20,10 +19,10 @@ public class DataStreamFileStorage extends FileStorage {
     @Override
     protected void write(OutputStream os, Resume resume) throws IOException { // создаем потоковы объект для записи данных в файл, оборачиваем объект классом для записи примитивных данных
         try (final DataOutputStream dos = new DataOutputStream(os)) {
-            writeString(dos, resume.getUuid());
-            writeString(dos, resume.getFullName());// записываем данные ФИО объекта резюме в файл
-            writeString(dos, resume.getLocation());
-            writeString(dos, resume.getHomePage());
+            dos.writeUTF(resume.getUuid());
+            dos.writeUTF(resume.getFullName());// записываем данные ФИО объекта резюме в файл
+            dos.writeUTF(resume.getLocation());
+            dos.writeUTF(resume.getHomePage());
 
             Map<ContactType, String> contacts = resume.getContacts(); // создаем коллекцию типа MAP, в которую помещаем контакты объекта резюму
 //            dos.writeInt(contacts.size()); // Раньше записывали в файл количество контактов, объекта резюме.
@@ -35,7 +34,7 @@ public class DataStreamFileStorage extends FileStorage {
                 // который обращается к методу интерфейса ElementWriter, который мы здесь переопределяем с помощью анонимного класса
                 public void write(Map.Entry<ContactType, String> entry) throws IOException {// где явно указываем тип коллекции
                     dos.writeInt(entry.getKey().ordinal()); //  и записываем ее данные (ключ и значение)
-                    writeString(dos, entry.getValue());
+                    dos.writeUTF(entry.getValue());
                 }
             });
 
@@ -47,25 +46,22 @@ public class DataStreamFileStorage extends FileStorage {
             Map<SectionType, Section> sections = resume.getSections();
             int sectionSize = sections.size();
             dos.writeInt(sections.size());
+
             for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
                 SectionType type = entry.getKey();
                 Section section = entry.getValue();
-                writeString(dos, type.name());
+                dos.writeUTF(type.name());
 
                 switch (type) {
                     case OBJECTIVE:
-                        writeString(dos, ((TextSection) section).getValue());
+                        dos.writeUTF(((TextSection) section).getValue());
                         break;
                     case ACHIVEMENT:
                     case QUALIFICATIONS:
                         // реализация через лямбда:
                         // writeCollection(dos, ((MultiTextSection) section).getValues(), value -> writeString(dos, value));
-                        writeCollection(dos, ((MultiTextSection) section).getValues(), new ElementWriter<String>() {
-                            @Override
-                            public void write(String value) throws IOException {
-                                writeString(dos, value);
-                            }
-                        });
+                        writeCollection(dos, ((MultiTextSection) section).getValues(), dos::writeUTF);
+                        break;
                     case EDUCATION:
                     case EXPIRIENCE:
                         writeCollection(dos, ((OrganizationSection) section).getValues(), (org) -> {
@@ -91,32 +87,32 @@ public class DataStreamFileStorage extends FileStorage {
     protected Resume read(InputStream is) throws IOException {
         Resume r = new Resume();
         try (DataInputStream dis = new DataInputStream(is)) {
-            r.setUuid(readString(dis));
-            r.setFullName(readString(dis));
-            r.setLocation(readString(dis));
-            r.setHomePage(readString(dis));
+            r.setUuid(dis.readUTF());
+            r.setFullName(dis.readUTF());
+            r.setLocation(dis.readUTF());
+            r.setHomePage(dis.readUTF());
             int contactSize = dis.readInt();
 
             for (int i = 0; i < contactSize; i++) {
-                r.addContact((ContactType.VALUES[dis.readInt()]), readString(dis));
+                r.addContact((ContactType.VALUES[dis.readInt()]), dis.readUTF());
             }
             final int sectionSize = dis.readInt();
 
             for (int i = 0; i < sectionSize; i++) {
-                SectionType sectionType = SectionType.valueOf(readString(dis));
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
                     case OBJECTIVE:
-                        r.addObjective(readString(dis));
+                        r.addObjective(dis.readUTF());
                         break;
                     case ACHIVEMENT:
                     case QUALIFICATIONS:
-                        r.addSection(sectionType, new MultiTextSection(readList(dis, () -> readString(dis))));
+                        r.addSection(sectionType, new MultiTextSection(readList(dis, dis::readUTF)));
                         break;
                     case EDUCATION:
                     case EXPIRIENCE:
                         r.addSection(sectionType,
-                                new OrganizationSection(readList(dis, () -> new Organization(new Link(dis.readUTF(),dis.readUTF()),
-                                        readList(dis, ()-> new Organization.Period(readLocalDate(dis),readLocalDate(dis), dis.readUTF(), dis.readUTF()))))));
+                                new OrganizationSection(readList(dis, () -> new Organization(new Link(dis.readUTF(), dis.readUTF()),
+                                        readList(dis, () -> new Organization.Period(readLocalDate(dis), readLocalDate(dis), dis.readUTF(), dis.readUTF()))))));
                         break;
 
 
@@ -128,14 +124,14 @@ public class DataStreamFileStorage extends FileStorage {
 
     }
 
-
-    private void writeString(DataOutputStream dos, String str) throws IOException { // эти методы созданы для проверки записываемых значений объекта на ноль
-        dos.writeUTF(str == null ? NULL : str);
+    private void writeLocalDate(DataOutputStream dos, LocalDate ld) throws IOException {
+        Objects.requireNonNull(ld, "Local date can not be null, use Period.NOW");
+        dos.writeInt(ld.getYear());
+        dos.writeUTF(ld.getMonth().name());
     }
 
-    private String readString(DataInputStream dis) throws IOException { // эти методы созданы для проверки записываемых значений объекта на ноль
-        String str = dis.readUTF();
-        return str.equals(NULL) ? null : str;
+    private LocalDate readLocalDate(DataInputStream dis) throws IOException {
+        return LocalDate.of(dis.readInt(), Month.valueOf(dis.readUTF()), 1);
     }
 
     private interface ElementWriter<T> { // внутренний интерфейс содержащий метод write, типизированный дженериком Т
@@ -170,14 +166,6 @@ public class DataStreamFileStorage extends FileStorage {
         return list; // в Java 1.8 можно заменить использованием лямбда выражений
     }
 
-    private void writeLocalDate(DataOutputStream dos, LocalDate ld) throws IOException {
-        Objects.requireNonNull(ld, "Local date can not be null, use Period.NOW");
-        dos.writeInt(ld.getYear());
-        dos.writeUTF(ld.getMonth().name());
-    }
 
-    private LocalDate readLocalDate(DataInputStream dis) throws IOException{
-        return LocalDate.of(dis.readInt(), Month.valueOf(dis.readUTF()),1);
-    }
 
 }
